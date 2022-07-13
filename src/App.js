6112e7dd9471e135af266cd231ab199eec25dd49
manan228 <manan228@yahoo.in>
2022-07-13 21:03:24 +0530

    Creating a Restaurant Website Task 5

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './Components/Layout/Header'
 import Meals from './Components/Meals/Meals';
+import Cart from './Components/Cart/Cart';
 function App() {
   return (
     <>
+    <Cart />
       <Header />
       <main>
         <Meals />
