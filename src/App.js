b1c83131a03b5b440e98c99e8704d4c9b6b17f08
manan228 <manan228@yahoo.in>
2022-07-13 21:19:52 +0530

    Creating a Restaurant Website Task 6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import Header from './Components/Layout/Header'
-import Meals from './Components/Meals/Meals';
-import Cart from './Components/Cart/Cart';
+import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
+import Cart from "./Components/Cart/Cart";
+import { useState } from "react";
+
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+
   return (
     <>
-    <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
